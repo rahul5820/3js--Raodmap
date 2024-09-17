bb3e84fb2549c9047fc5eed03c9bbac6b1b2a1e9
Rahul <Rr8305820@gmail.com>
2024-09-17 21:54:13 +0530

    day 1 learn

--- script.js
+++ script.js
@@ -1,38 +1,49 @@
-// class canvasPaint {
-//     constructor(data) {
-//         this.name="rahul"
-//         this.age=34; 
-//         this.product={
-//             nameP:"rahul",
-//             branch:"mca"
-//         };
-//         this.data=data
+// // class canvasPaint {
+// //     constructor(data) {
+// //         this.name="rahul"
+// //         this.age=34; 
+// //         this.product={
+// //             nameP:"rahul",
+// //             branch:"mca"
+// //         };
+// //         this.data=data
+// //     }
+// //     PrintDetails(){
+// //         console.log(this.data,"gettin age "+this.age ,"gettin name "+this.name)
+// //     }
+// // }
+// // let canvas= new canvasPaint(12);
+// // canvas.PrintDetails();
+
+// class Bubble{
+//    constructor(parent,num) {
+//    this.parent=parent;
+//    this.num=num;     
 //     }
-//     PrintDetails(){
-//         console.log(this.data,"gettin age "+this.age ,"gettin name "+this.name)
+//    AddBubble(){
+//     for(var i=1;i<=this.num;i++){
+//          var Bubbles=document.createElement("div");
+//          Bubbles.classList.add("bubble")
+//          Bubbles.textContent=i;
+
+//         document.querySelector(this.parent).appendChild(Bubbles)
 //     }
-// }
-// let canvas= new canvasPaint(12);
-// canvas.PrintDetails();
+//    }
+//    }
 
-class Bubble{
-   constructor(parent,num) {
-   this.parent=parent;
-   this.num=num;     
-    }
-   AddBubble(){
-    for(var i=1;i<=this.num;i++){
-         var Bubbles=document.createElement("div");
-         Bubbles.classList.add("bubble")
-         Bubbles.textContent=i;
-
-        document.querySelector(this.parent).appendChild(Bubbles)
-    }
-   }
-   }
-
-   let PrintBubble= new Bubble(".app",10);
-   PrintBubble.AddBubble();
+//    let PrintBubble= new Bubble(".app",10);
+//    PrintBubble.AddBubble();
 
 
+const Array =[3,45,5,7,4]
 
+let output=Array.filter((num)=> num%5 ==0)
+ 
+// but if we use scope then we have to use return statement
+
+let numF=Array.filter((num)=>{
+  return  num>5
+})
+
+console.log(numF)
+console.log(output)
